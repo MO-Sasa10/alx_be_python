21931e6dff02cf8b94eed2fa177e55a804886667
MO-Sasa10 <mostafa_elashry19@icloud.com>
2025-06-06 20:20:46 +0300

    Create  pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,15 @@
+try:
+    size = int(input("Enter the size of the pattern: "))
+
+    if size > 0:
+        row = 0
+        while row < size:
+            for col in range(size):
+                print("*", end="")
+            print()  # Move to the next line after printing one row
+            row += 1
+    else:
+        print("Please enter a positive integer.")
+
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
